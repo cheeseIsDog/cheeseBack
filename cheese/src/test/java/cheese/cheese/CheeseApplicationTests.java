26f2fb24c2be7b12b41883c6f40e76757a48952e
최기환 <directorhwan@naver.com>
2021-12-06 20:43:57 +0900

    first commit

---
+++ cheese/src/test/java/cheese/cheese/CheeseApplicationTests.java
@@ -0,0 +1,13 @@
+package cheese.cheese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
