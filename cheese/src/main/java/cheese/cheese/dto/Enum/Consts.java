5011b2fb277cb26837a3243357fc7003eba8f5f1
최기환 <directorhwan@naver.com>
2022-06-25 12:21:58 +0900

    [ADD] Email Template 추가

--- cheese/src/main/java/cheese/cheese/dto/Enum/Consts.java
+++ cheese/src/main/java/cheese/cheese/dto/Enum/Consts.java
@@ -1,19 +1,6 @@
 package cheese.cheese.dto.Enum;
 
 public final class Consts {
-    public static final String EMAIL_MESSAGE_START = "<div style='margin:100px;'>"
-            + "<h1> 안녕하세요  Cheeze입니다!!! </h1>"
-            + "<br><p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>"
-            + "<br>"
-            + "<p>감사합니다!<p>"
-            + "<br>"
-            + "<div align='center' style='border:1px solid black; font-family:verdana';>"
-            + "<h3 style='color:blue;'>회원가입 코드입니다.</h3>"
-            + "<div style='font-size:130%'>"
-            + "CODE : <strong>";
-
-    public static final String EMAIL_MESSAGE_END = "</strong><div><br/> "
-            + "</div>";
 
     public static final String BLANK = "";
 
@@ -25,4 +12,81 @@ public final class Consts {
     public static final String DIS_LIKE = "disLike";
     public static final String DO_NOTHING = "doNothing";
 
+
+    public static final String EMAIL_MESSAGE_START = "<html>\n" +
+            "  <head>\n" +
+            "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" +
+            "    <title>개발멍발 회원가입 인증 메일</title>\n" +
+            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
+            "  </head>\n" +
+            "  <body>\n" +
+            "    <table align=\"center\">\n" +
+            "      <tr>\n" +
+            "        <td align=\"center\"><h1>개발멍발 회원 가입 인증 메일</h1></td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <br />\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <br />\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <img style=\"float: left\" width=\"40\" src=\"https://cheesedev.xyz/favicon.ico\" alt=\"\" />\n" +
+            "          <span style=\"font-size: 24px; font-weight: 600; line-height: 48px\">개발멍발</span>\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <br />\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <h2>이메일 주소 인증</h2>\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <br />\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td style=\"border-top: 2px solid black\"></td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <br />\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>안녕하세요!</td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <br />\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>개발멍발 회원 가입을 위한 인증 이메일입니다.</td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>아래 인증 번호를 이용하여 인증을 해주세요!</td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>\n" +
+            "          <br />\n" +
+            "        </td>\n" +
+            "      </tr>\n" +
+            "      <tr>\n" +
+            "        <td>인증번호 : ";
+    public static final String EMAIL_MESSAGE_END = "</td>\n" +
+            "      </tr>\n" +
+            "    </table>\n" +
+            "  </body>\n" +
+            "</html>";
 }
